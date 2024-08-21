e3bff1249d910341a067f6f1a3b43406152c774e
Yaswanth1320 <yashwanth.paravathala@gmail.com>
2024-08-21 17:16:52 +0530

    Auth page setup

---
+++ src/app/auth/callback/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+  return (
+    <div>
+      auth call back
+    </div>
+  );
+}
+
+export default page;
---
+++ src/app/auth/page.tsx
@@ -0,0 +1,11 @@
+
+const page = () => {
+  return (
+    <div className="flex h-screen w-full">
+      <div className="flex-1 flex overflow-hidden dark:bg-[#651c2b55] bg-[#651c2b]"></div>
+      <div></div>
+    </div>
+  );
+}
+
+export default page;
