bd7b26adbbfcb9da1a31d1ac5701730360b64083
Yaswanth1320 <yashwanth.paravathala@gmail.com>
2025-01-04 21:50:39 +0530

    message send and get

--- src/components/chat/ChatBottom.tsx
+++ src/components/chat/ChatBottom.tsx
@@ -12,18 +12,47 @@ import EmojiPicker from "./EmojiPicker";
 import { Button } from "../ui/button";
 import useSound from "use-sound";
 import { usePreference } from "@/store/usePreferences";
+import { useMutation } from "@tanstack/react-query";
+import { sendMessageAction } from "@/actions/message.action";
+import { useSelectedUser } from "@/store/useSelectedUser";
 
 const ChatBottom = () => {
   const [message, setMessage] = useState("");
   const { soundEnabled } = usePreference();
+  const { selectedUser } = useSelectedUser();
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
-  const isPending = false;
   const [playSound1] = useSound("/sounds/keystroke1.mp3");
   const [playSound2] = useSound("/sounds/keystroke2.mp3");
   const [playSound3] = useSound("/sounds/keystroke3.mp3");
   const [playSound4] = useSound("/sounds/keystroke4.mp3");
-
   const soundsArray = [playSound1, playSound2, playSound3, playSound4];
+
+  const { mutate: sendMessage, isPending } = useMutation({
+    mutationFn: sendMessageAction,
+  });
+
+  const handleSendMessage = () => {
+    if (!message.trim()) return;
+    console.log("inside mess");
+    sendMessage({
+      content: message,
+      messageType: "text",
+      receiverId: selectedUser?.id!,
+    });
+    setMessage("");
+    textAreaRef.current?.focus();
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === "Enter" && !e.shiftKey) {
+      e.preventDefault();
+      handleSendMessage();
+    }
+    if (e.key === "Enter" && e.shiftKey) {
+      e.preventDefault();
+      setMessage(message + "/n");
+    }
+  };
 
   const playRandomKeyStroke = () => {
     const randomIndex = Math.floor(Math.random() * soundsArray.length);
@@ -55,6 +84,7 @@ const ChatBottom = () => {
             placeholder="Aa"
             rows={1}
             value={message}
+            onKeyDown={handleKeyDown}
             onChange={(e) => {
               setMessage(e.target.value);
               playRandomKeyStroke();
@@ -78,6 +108,7 @@ const ChatBottom = () => {
             className="w-9 h-9 dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white shrink-0"
             variant={"ghost"}
             size={"icon"}
+            onClick={handleSendMessage}
           >
             <SendHorizonal size={20} className="text-muted-foreground" />
           </Button>
@@ -88,7 +119,17 @@ const ChatBottom = () => {
             size={"icon"}
           >
             {!isPending && (
-              <ThumbsUp size={20} className="text-muted-foreground" />
+              <ThumbsUp
+                size={20}
+                className="text-muted-foreground"
+                onClick={() => {
+                  sendMessage({
+                    content: "👍",
+                    messageType: "text",
+                    receiverId: selectedUser?.id!,
+                  });
+                }}
+              />
             )}
             {isPending && <Loader size={20} className="animate-spin" />}
           </Button>
